94a950b737da878a62df3bc7d20a2492b3c0daf3
erolando <erolandoaq@gmail.com>
2021-11-27 17:15:09 -0600

    Primera version

---
+++ HomeDepot/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeDepot.Startup))]
+namespace HomeDepot
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
