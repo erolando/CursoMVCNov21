cbd66123a95ddaeeed9f93ad88d7c9bc163df4d4
erolando <erolandoaq@gmail.com>
2021-12-04 14:22:28 -0600

    CReacion de prueda unitaria para repositorio de productos

---
+++ HomeDepot.Tests/Repository/UnitTestRepositoryProducts.cs
@@ -0,0 +1,32 @@
+﻿using HomeDepot.DataLayer.Entities;
+using HomeDepot.DataLayer.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace HomeDepot.Tests.Repository
+{
+    [TestClass]
+    public class UnitTestRepositoryProducts
+    {
+        private readonly RepositoryProducts Repository;
+        public UnitTestRepositoryProducts()
+        {
+            Repository = new RepositoryProducts();
+        }
+        [TestMethod]
+        public void TestGetAll()
+        {
+            List<ModelProduct> results = Repository.GetAll();
+
+            Assert.IsTrue(results.Count > 0);
+
+            foreach (ModelProduct product in results)
+            {
+                Assert.IsTrue(product.Nombre.Length > 0);
+                Assert.IsNotNull(product.IdCategoriaProducto);
+                Assert.IsFalse(product.IdCategoriaProducto < 0);
+            }
+        }
+    }
+}
